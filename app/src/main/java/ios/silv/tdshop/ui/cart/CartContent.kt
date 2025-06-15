26adf9739ea1909a047da8dbfbcf91c9a2f95a06
SilvVF <david.silver232@gmail.com>
2025-06-15 17:20:05 -0400

    cart details shared element

--- app/src/main/java/ios/silv/tdshop/ui/cart/CartContent.kt
+++ app/src/main/java/ios/silv/tdshop/ui/cart/CartContent.kt
@@ -1,15 +1,28 @@
 package ios.silv.tdshop.ui.cart
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,15 +31,22 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.data.Group
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation3.runtime.EntryProviderBuilder
 import androidx.navigation3.runtime.entry
 import androidx.navigation3.ui.LocalNavAnimatedContentScope
@@ -37,7 +57,9 @@ import ios.silv.tdshop.nav.Ship
 import ios.silv.tdshop.types.UiCartItem
 import ios.silv.tdshop.types.UiProduct
 import ios.silv.tdshop.ui.components.QtyIndicator
+import ios.silv.tdshop.ui.compose.EventEffect
 import ios.silv.tdshop.ui.compose.MutedAlpha
+import ios.silv.tdshop.ui.compose.rememberEventFlow
 import ios.silv.tdshop.ui.home.cartPreviewData
 import ios.silv.tdshop.ui.home.previewUiProducts
 import ios.silv.tdshop.ui.theme.TdshopTheme
@@ -87,36 +109,125 @@ fun EntryProviderBuilder<Screen>.cartScreenEntry(
                 }
             }
         ) { paddingValues ->
-            // TODO: implement shared element preview https://developer.android.com/develop/ui/compose/animation/shared-elements#animated-visibility
             CartContent(
                 state = state,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
+                modifier = Modifier.fillMaxSize()
             )
         }
     }
 }
 
 @Composable
-private fun CartContent(
+private fun SharedTransitionScope.CartContent(
     state: CartState,
     modifier: Modifier
 ) {
+    BackHandler(state.selectedItem != null) {
+        state.events(CartEvent.ViewProduct(null))
+    }
+
     LazyColumn(modifier.fillMaxSize()) {
         items(state.groupedItems) { (item, product, variant) ->
-            CartListItem(
-                item,
-                product,
-                variant,
-                state.events,
-                Modifier
-                    .padding(horizontal = 2.dp)
-                    .height(120.dp)
-            )
+            AnimatedVisibility(
+                visible = variant.id != state.selectedItem?.third?.id,
+                enter = fadeIn() + scaleIn(),
+                exit = fadeOut() + scaleOut(),
+                modifier = Modifier.animateItem()
+            ) {
+                CartListItem(
+                    item,
+                    product,
+                    variant,
+                    state.events,
+                    Modifier
+                        .heightIn(min = 120.dp, max = 240.dp)
+                        .sharedElement(
+                            sharedContentState = rememberSharedContentState(key = variant.id),
+                            animatedVisibilityScope = this@AnimatedVisibility
+                        )
+                        .padding(horizontal = 2.dp)
+                        .combinedClickable(
+                            onLongClick = {
+                                state.events(
+                                    CartEvent.ViewProduct(
+                                        Triple(
+                                            item,
+                                            product,
+                                            variant
+                                        )
+                                    )
+                                )
+                            },
+                            onClick = {}
+                        )
+                )
+            }
+        }
+    }
+    ProductDetails(state)
+}
+
+@Composable
+private fun SharedTransitionScope.ProductDetails(
+    state: CartState,
+    modifier: Modifier = Modifier,
+) {
+    AnimatedContent(
+        modifier = modifier,
+        targetState = state.selectedItem,
+        transitionSpec = {
+            fadeIn() togetherWith fadeOut()
+        },
+        label = "ProductDetails"
+    ) { selected ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            if (selected != null) {
+                val (item, product, variant) = selected
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.5f))
+                        .clickable {
+                            state.events(CartEvent.ViewProduct(null))
+                        }
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .sharedBounds(
+                            sharedContentState = rememberSharedContentState(key = variant.id),
+                            animatedVisibilityScope = this@AnimatedContent,
+                            clipInOverlayDuringTransition = OverlayClip(RectangleShape)
+                        )
+                        .clip(RectangleShape)
+                        .clickable{}
+                ) {
+                    CartListItem(
+                        item,
+                        product,
+                        variant,
+                        state.events,
+                        Modifier
+                            .background(
+                                MaterialTheme.colorScheme.background,
+                                RectangleShape
+                            )
+                            .fillMaxHeight(0.5f)
+                            .fillMaxWidth()
+                            .sharedBounds(
+                                sharedContentState = rememberSharedContentState(key = variant.id),
+                                animatedVisibilityScope = this@AnimatedContent,
+                            )
+                    )
+                }
+            }
         }
     }
 }
+
 
 @Composable
 fun CartListItem(
@@ -184,15 +295,29 @@ fun CartListItem(
 private fun PreviewCartContent() {
     TdshopTheme {
         val backStack = LocalBackStack.current
-        val state by remember {
+        val events = rememberEventFlow<CartEvent>()
+
+        var state by remember {
             mutableStateOf(
                 CartState(
                     cart = cartPreviewData,
                     products = previewUiProducts,
-                    events = {}
+                    selectedItem = null,
+                    events = events::tryEmit
                 )
             )
         }
+
+        EventEffect(events) {
+            when (it) {
+                is CartEvent.ViewProduct -> state = state.copy(
+                    selectedItem = it.item
+                )
+
+                else -> Unit
+            }
+        }
+
         SharedTransitionLayout {
             AnimatedVisibility(true) {
                 rememberScaffoldState(
--- app/src/main/java/ios/silv/tdshop/ui/cart/CartPresenter.kt
+++ app/src/main/java/ios/silv/tdshop/ui/cart/CartPresenter.kt
@@ -1,8 +1,11 @@
 package ios.silv.tdshop.ui.cart
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import ios.silv.tdshop.di.requireActivityComponent
 import ios.silv.tdshop.net.CartRepo
 import ios.silv.tdshop.net.ProductRepo
@@ -14,8 +17,11 @@ import ios.silv.tdshop.ui.compose.providePresenterDefaults
 import ios.silv.tdshop.ui.compose.rememberEventFlow
 import me.tatarka.inject.annotations.Inject
 
+typealias GroupedItem = Triple<UiCartItem, UiProduct, UiProduct.Variant>
+
 data class CartState(
     val cart: UiCart,
+    val selectedItem: GroupedItem?,
     val products: List<UiProduct>,
     val events: (CartEvent) -> Unit
 ) {
@@ -26,7 +32,7 @@ data class CartState(
         .groupBy { it.second.id }
         .mapValues { it.value.first() }
 
-    val groupedItems: List<Triple<UiCartItem, UiProduct, UiProduct.Variant>> =
+    val groupedItems: List<GroupedItem> =
         cart.items.mapNotNull { item ->
             val (product, variant) = variantIdToProduct[item.productVariantId]
                 ?: return@mapNotNull null
@@ -40,6 +46,8 @@ sealed interface CartEvent {
     data class DecQty(val item: UiCartItem) : CartEvent
     data class IncQty(val item: UiCartItem) : CartEvent
     data class Remove(val item: UiCartItem) : CartEvent
+    data object Clear: CartEvent
+    data class ViewProduct(val item: GroupedItem?): CartEvent
 }
 
 @Composable
@@ -59,17 +67,31 @@ fun cartPresenterProvider(
     val cart = cartRepo.cart()
     val products = productRepo.products()
 
-    EventEffect(events) {
-        when (it) {
-            else -> {
+    var selectedItem by remember { mutableStateOf<GroupedItem?>(null) }
 
+    EventEffect(events) {event ->
+        when (event) {
+            CartEvent.Clear -> cartRepo.clear()
+            is CartEvent.DecQty ->  cartRepo.addItem {
+                productVariantId(event.item.productVariantId)
+                quantity((event.item.quantity - 1).coerceAtLeast(0))
             }
+            is CartEvent.IncQty -> cartRepo.addItem {
+                productVariantId(event.item.productVariantId)
+                quantity(event.item.quantity + 1)
+            }
+            is CartEvent.Remove -> cartRepo.addItem {
+                productVariantId(event.item.productVariantId)
+                quantity(0)
+            }
+            is CartEvent.ViewProduct -> selectedItem = event.item
         }
     }
 
     CartState(
         cart = cart,
-        products = products
+        products = products,
+        selectedItem = selectedItem
     ) {
         events.tryEmit(it)
     }
